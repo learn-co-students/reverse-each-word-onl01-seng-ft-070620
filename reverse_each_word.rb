0b9e82ee668e1da8724669ed6389a562df3d32b1
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-15 16:27:09 -0700

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,23 +1,23 @@
-### code below
 
+## REWORK
 
-# using .collect method:
+# 1. Build a method #reverse_each_word that:
+#    -Takes in a sentence argument
+#    -Returns the same sentence with each word reversed
 
-def reverse_each_word(phrase)
-    phrase.split.collect do |word| 
+def reverse_each_word(sentence)
+    sentence.split(" ").collect do |word|
         word.reverse
     end
-        .join(" ")
+    .join(" ")
 end 
 
+# # using .each method:
 
-
-# using .each method:
-
-# def reverse_each_word(phrase)
-#     new_array = []
-#     phrase.split.each do |word| 
-#         new_array << word.reverse
-#     end
-#         new_array.join(" ")
-# end 
+# # def reverse_each_word(phrase)
+# #     new_array = []
+# #     phrase.split.each do |word| 
+# #         new_array << word.reverse
+# #     end
+# #         new_array.join(" ")
+# # end 
